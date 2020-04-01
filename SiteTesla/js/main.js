245e61bb4aade0779f7c17c412ab6750d96a6d3f
Billy_Bones <60989043+Billy-BonesUA@users.noreply.github.com>
2020-04-01 21:50:00 +0300

    Add files via upload

--- SiteTesla/js/main.js
+++ SiteTesla/js/main.js
@@ -2,6 +2,14 @@ $(function(){
 	$('.slider').slick({
 		arrows:false,
 		fade:true,
-		autoplay:30
+		autoplay:3000,
+		dots:true
 	});
+// Ищем класс с кнопкой
+$('.header-btn').on('click', function(){
+	$('.menu').addClass('active');
+})
+$('.close-btn').on('click', function(){
+	$('.menu').removeClass('active');
+})
 });
